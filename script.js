43e8b4e315844377116c5cf4e05772658e469590
ELIONARDO MIRANDA DINIZ FILHO <191672023@ETEC.BR>
2024-03-28 11:06:56 -0300

    API Fetch test

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function funcao(){
-    console.log("script criado!")
+const openmeteoUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m';
+
+
+const CallAPI = async (apiUrl) => {
+    await fetch(apiUrl)
+    .then(response => {
+        if(!response.ok){
+            throw new Error('erro de response');
+        }
+        return response.json();
+    })
+    .then(data => {
+
+    })
 }
