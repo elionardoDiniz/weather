118b6c46d454388353d65f8e6b93e6fd809d82d4
HeizoAlencar <128201506+HeizoAlencar@users.noreply.github.com>
2024-04-12 11:41:22 +0000

    tetse

--- src/News.js
+++ src/News.js
@@ -12,9 +12,9 @@ const News = () => {
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const apiKey = "ed0ee08d5ad243069218b7efeedd62cb";
-        const url = `https://newsapi.org/v2/everything?q=meteorologia&sortBy=publishedAt&pageSize=6&language=pt&apiKey=${apiKey}`;
-        
+        const apiKey = "f27fe093a4d645c7a14a319a43159c82";
+        const url = `https://newsapi.org/v2/everything?q=meteorologia&sortBy=publishedAt&pageSize=9&language=pt&apiKey=${apiKey}`;
+
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error("Falha ao carregar os dados");
@@ -34,53 +34,48 @@ const News = () => {
   }, [articles]);
 
   return (
-    <Container>
-      <Carousel heigh={'90vh'} theme="dark">
-        {articles.map((article, index) => (
-          <Carousel.Item key={index} link={article.url} >
-            <img
-            
-              className="d-block w-100   mh-100 d-inline-block img-fluid mh-100 " 
-              src={article.urlToImage}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <div className="carouselCaptionBg" >
-              { article.source && <p >{article.source.name}</p>}
-              { article.title && <h5 className="carouselTextColor" >{article.title}</h5>}
-              { article.description && <p className="carouselTextColor" >{article.description}</p>}
-              </div>
-              
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-      
-      <ul className="newsList" >
-      <hr></hr>
-      <h1 >Noticias</h1>
-      {
-        articles.map((article, index) => (
+    <>
+      <h1 className="newsTitle">
+        <i class="fa-solid fa-newspaper"></i> Noticias
+      </h1>
+      <Container>
+        <Carousel heigh={"90vh"} theme="dark">
+          {articles.map((article, index) => (
+            <Carousel.Item key={index} link={article.url}>
+              <img
+                class="carosuel"
+                className="d-block w-100   mh-100 d-inline-block img-fluid mh-100 "
+                src={article.urlToImage}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <div className="carouselCaptionBg">
+                  {article.source && <p>{article.source.name}</p>}
+                  {article.title && (
+                    <h5 className="carouselTextColor">{article.title}</h5>
+                  )}
+                  {article.description && (
+                    <p className="carouselTextColor">{article.description}</p>
+                  )}
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
 
-          <li>
-            {article.content && <hr></hr>}
-            <Link to={article.url} >
-              
-              {article.title && <h2>{article.title}</h2>}
-              {article.description && <p>{article.description}</p>}
-
-            
-            </Link>
-            
-          </li>
-  
-        ))
-      }
-      </ul>
-      
-    </Container>
-      
-    
+        <ul className="newsList">
+          {articles.map((article, index) => (
+            <li>
+              {article.content && <hr></hr>}
+              <Link to={article.url}>
+                {article.title && <h2>{article.title}</h2>}
+                {article.description && <p>{article.description}</p>}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </>
   );
 };
 
